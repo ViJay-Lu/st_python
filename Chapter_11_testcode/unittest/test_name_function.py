53dd458450bfb6411c5164a4f8f945174f89f431
ViJay <1047527105@qq.com>
2024-10-11 17:02:15 +0800

    add-new st_code

--- Chapter_11_testcode/unittest/test_name_function.py
+++ Chapter_11_testcode/unittest/test_name_function.py
@@ -10,7 +10,7 @@ class TestNameFunction(unittest.TestCase):
     def test_full_name_by_age(self):
         full_name = get_full_name_by_age('lu','vijay',32)
         self.assertEqual(full_name, 'lu vijay 32 years old')
-#书中的写法为：
+#书中的写法为:
 # unittest.main()
 #这样会报错。AttributeError: module '__main__' has no attribute 'test_name_function'
 
